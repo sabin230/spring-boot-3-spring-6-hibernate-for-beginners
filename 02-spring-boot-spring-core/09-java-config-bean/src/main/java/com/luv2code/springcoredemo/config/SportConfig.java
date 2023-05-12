22446000ddafa8863a4c2bb09248b1845933c054
sabin230 <sabinpradhan@gmail.com>
2023-05-12 13:48:47 -0400

    Update SportConfig.java

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/config/SportConfig.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/config/SportConfig.java
@@ -4,11 +4,11 @@ import com.luv2code.springcoredemo.common.Coach;
 import com.luv2code.springcoredemo.common.SwimCoach;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// SwimCoach does not have @Component so Configuration annotation with @Bean is used for dependency injection
 @Configuration
 public class SportConfig {
 
-    @Bean("aquatic")
+    @Bean("aquatic") // By defaul bean id is method Name. However, in this case it is aquatic. notice method name are lowercase
     public Coach swimCoach() {
         return new SwimCoach();
     }
