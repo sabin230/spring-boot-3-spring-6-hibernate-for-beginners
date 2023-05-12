c7ae3d950b5a9a25d15466cb6b2b5014d3407621
sabin230 <sabinpradhan@gmail.com>
2023-05-12 13:30:09 -0400

    Added comment

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,6 +3,7 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// using custom application property . use http://localhost:7070/mycoolapp/teaminfo
 @SpringBootApplication
 public class MycoolappApplication {
 
